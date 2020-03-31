6eec2c93e3cd565bd90ccf713de6a989a06d4b3c
Pavel Ostroukhov <pavel.ostroukhov@gmail.com>
2020-04-01 00:14:01 +0400

    Added d3d device init

--- DirectXGame/AppWindow.cpp
+++ DirectXGame/AppWindow.cpp
@@ -2,13 +2,17 @@
 
 void AppWindow::onCreate()
 {
+	Window::onCreate();
+	GraphicsEngine::get()->init();
 }
 
 void AppWindow::onUpdate()
 {
+	Window::onUpdate();
 }
 
 void AppWindow::onDestroy()
 {
 	Window::onDestroy();
+	GraphicsEngine::get()->release();
 }
--- DirectXGame/Window.h
+++ DirectXGame/Window.h
@@ -12,8 +12,8 @@ public:
 	bool isRun();
 
 	// Events
-	virtual void onCreate()=0;
-	virtual void onUpdate()=0;
+	virtual void onCreate();
+	virtual void onUpdate();
 	virtual void onDestroy();
 protected:
 	HWND m_hwnd;
