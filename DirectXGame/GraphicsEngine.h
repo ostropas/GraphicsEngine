88e94be3ee3e94492aeeee021a15680232d4850e
Pavel Ostroukhov <pavel.ostroukhov@gmail.com>
2020-04-01 00:14:01 +0400

    Added d3d device init

---
+++ DirectXGame/GraphicsEngine.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <d3d11.h>
+
+class GraphicsEngine
+{
+public:
+	// Initialize the GraphicsEngine and Directx11 Device
+	bool init();
+	// Release all resources loaded
+	bool release();
+public:
+	static GraphicsEngine* get();
+private:
+	ID3D11Device* m_d3d_device;
+	D3D_FEATURE_LEVEL m_feature_level;
+	ID3D11DeviceContext* m_imm_context;
+};
+
--- DirectXGame/Window.cpp
+++ DirectXGame/Window.cpp
@@ -84,6 +84,14 @@ bool Window::isRun()
 	return m_is_run;
 }
 
+void Window::onCreate()
+{
+}
+
+void Window::onUpdate()
+{
+}
+
 void Window::onDestroy()
 {
 	m_is_run = false;
