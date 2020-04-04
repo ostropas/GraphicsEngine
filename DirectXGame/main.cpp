f71d18ff0c6f814a1041d623e39610e0beb31d80
Pavel Ostroukhov <pavel.ostroukhov@gmail.com>
2020-04-04 23:48:43 +0400

    change application to win application

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -1,8 +1,10 @@
 #include "AppWindow.h"
+#include <wchar.h>
+#include <stdlib.h>
 
 // https://www.youtube.com/playlist?list=PLv8DnRaQOs5-ST_VDqgbbMRtzMtpK36Hy
 
-int main() {
+int wmain(int argc, wchar_t* argv[], wchar_t* envp[]) {
 	AppWindow app;
 	if (app.init()) {
 
