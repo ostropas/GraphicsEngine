818bd26f74e0e60f190cc817c6345bf414ecef4d
Pavel Ostroukhov <pavel.ostroukhov@gmail.com>
2020-04-04 23:44:38 +0400

    Draw triangle

--- DirectXGame/AppWindow.cpp
+++ DirectXGame/AppWindow.cpp
@@ -1,5 +1,15 @@
 #include "AppWindow.h"
 
+struct  vec3
+{
+	float x, y, z;
+};
+
+struct vertex
+{
+	vec3 position;
+};
+
 void AppWindow::onCreate()
 {
 	Window::onCreate();
@@ -8,6 +18,23 @@ void AppWindow::onCreate()
 
 	RECT rc = this->getClientWindowRect();
 	m_swap_chain->init(this->m_hwnd, rc.right-rc.left, rc.bottom-rc.top);
+
+	vertex list[] = {
+		{-0.5f, -0.5f, 0.0f},
+		{0.0f, 0.5f, 0.0f},
+		{0.5f, -0.5f, 0.0f}
+	};
+
+	m_vb = GraphicsEngine::get()->createVertexBuffer();
+	UINT size_list = ARRAYSIZE(list);
+
+	GraphicsEngine::get()->createShaders();
+
+	void* shader_byte_code = nullptr;
+	UINT size_shader = 0;
+	GraphicsEngine::get()->getShaderBufferAndSize(&shader_byte_code, &size_shader);
+
+	m_vb->load(list, sizeof(vertex), size_list, shader_byte_code, size_shader);
 }
 
 PingPongValue<float> r { 0, 1, 0.0002f, 0 };
@@ -22,6 +49,14 @@ void AppWindow::onUpdate()
 	r.step();
 	g.step();
 	b.step();
+
+	RECT rc = this->getClientWindowRect();
+	GraphicsEngine::get()->getImmediateDeviceContext()->setViewportSize(rc.right - rc.left, rc.bottom - rc.top);
+	GraphicsEngine::get()->setShaders();
+
+	GraphicsEngine::get()->getImmediateDeviceContext()->setVertexBuffer(m_vb);
+
+	GraphicsEngine::get()->getImmediateDeviceContext()->drawTriangleList(m_vb->getSizeVertexList(), 0);
 	
 	m_swap_chain->present(false);
 }
@@ -29,7 +64,7 @@ void AppWindow::onUpdate()
 void AppWindow::onDestroy()
 {
 	Window::onDestroy();
-
+	m_vb->release();
 	m_swap_chain->release();
 	GraphicsEngine::get()->release();
 }
