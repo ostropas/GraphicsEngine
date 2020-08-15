663d449f16763dcb00ff75bdf2f40fc5425382e9
Pavel Ostroukhov <pavel.ostroukhov@gmail.com>
2020-08-15 11:13:58 +0400

    Hide console windows

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -5,7 +5,7 @@
 int wmain(int argc, wchar_t* argv[], wchar_t* envp[]) {
 	AppWindow app;
 	if (app.init()) {
-
+		::ShowWindow(::GetConsoleWindow(), SW_HIDE);
 		while (app.isRun())
 		{
 			app.broadcast();
