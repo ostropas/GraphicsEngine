8410341396a80efba17c07f15b7b1a36f9705dc5
Pavel Ostroukhov <pavel.ostroukhov@gmail.com>
2020-08-23 13:25:30 +0400

    Update window architecture

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -10,13 +10,16 @@ int wmain(int argc, wchar_t* argv[], wchar_t* envp[]) {
 	}
 	catch (...) { return -1; }
 
-	AppWindow app;
-	if (app.init()) {
-		::ShowWindow(::GetConsoleWindow(), SW_HIDE);
-		while (app.isRun())
-		{
-			app.broadcast();
-		}
+	::ShowWindow(::GetConsoleWindow(), SW_HIDE);
+	try {
+		AppWindow app;
+		while (app.isRun());
+	}
+	catch (...) {
+		GraphicsEngine::release();
+		InputSystem::release();
+
+		return -1;
 	}
 
 	GraphicsEngine::release();
