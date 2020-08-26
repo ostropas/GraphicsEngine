4f0e057e83beae22df555f58ceaf312d060a1598
Pavel Ostroukhov <pavel.ostroukhov@gmail.com>
2020-08-27 00:43:53 +0400

    Lightning added

--- DirectXGame/AppWindow.cpp
+++ DirectXGame/AppWindow.cpp
@@ -22,7 +22,8 @@ struct constant
 	Matrix4x4 m_world;
 	Matrix4x4 m_view;
 	Matrix4x4 m_proj;
-	unsigned int m_time;
+	Vector4D m_light_direction;
+	Vector4D m_camera_position;
 };
 
 
@@ -33,17 +34,22 @@ AppWindow::AppWindow()
 void AppWindow::update()
 {
 	constant cc;
-	cc.m_time = ::GetTickCount();
-
 	m_delta_pos += m_delta_time / 10.0f;
 	if (m_delta_pos > 1.0f)
 		m_delta_pos = 0;
 
 
 	Matrix4x4 temp;
+	Matrix4x4 m_light_rot_matrix;
+	m_light_rot_matrix.setIdentity();
+	m_light_rot_matrix.setRotationY(m_light_rot_y);
 
-	/*
+	m_light_rot_y += 0.707f * m_delta_time;
+
+	cc.m_light_direction = m_light_rot_matrix.getZDirection();
+
 	m_delta_scale += m_delta_time / 0.55f;
+	/*
 
 	if (m_scale_cube != m_new_scale_cube) {
 		float add = m_scale_cube > m_new_scale_cube ? -m_delta_time : m_delta_time;
@@ -82,11 +88,13 @@ void AppWindow::update()
 	world_cam *= temp;
 
 
-	Vector3D new_pos = m_world_cam.getTranslation() + world_cam.getZDirection() * (m_forward * 0.3f);
-	new_pos += world_cam.getXDirection() * (m_rightward * 0.3f);
+	Vector3D new_pos = m_world_cam.getTranslation() + world_cam.getZDirection() * (m_forward * 0.01f);
+	new_pos += world_cam.getXDirection() * (m_rightward * 0.01f);
 
 
 	world_cam.setTranslation(new_pos);
+
+	cc.m_camera_position = new_pos;
 
 	m_world_cam = world_cam;
 
@@ -132,13 +140,13 @@ void AppWindow::onCreate()
 	InputSystem::get()->showCursor(false);
 
 	m_wood_tex = GraphicsEngine::get()->getTextureManager()->createTextureFromFile(L"Assets\\Textures\\brick.png");
-	m_mesh = GraphicsEngine::get()->getMeshManager()->createMeshFromFile(L"Assets\\Meshes\\teapot.obj");
+	m_mesh = GraphicsEngine::get()->getMeshManager()->createMeshFromFile(L"Assets\\Meshes\\statue.obj");
 
 	RECT rc = this->getClientWindowRect();
 	m_swap_chain = GraphicsEngine::get()->getRenderSystem()->createSwapChain(this->m_hwnd, rc.right - rc.left, rc.bottom - rc.top);
 
 
-	m_world_cam.setTranslation(Vector3D(0, 0, -2.0f));
+	m_world_cam.setTranslation(Vector3D(0, 0, -1.0f));
 
 	Vector3D position_list[] =
 	{
@@ -248,7 +256,6 @@ void AppWindow::onCreate()
 	GraphicsEngine::get()->getRenderSystem()->releaseCompiledShader();
 
 	constant cc;
-	cc.m_time = 0;
 
 	m_cb = GraphicsEngine::get()->getRenderSystem()->createConstantBuffer(&cc, sizeof(constant));
 }
--- DirectXGame/PixelShader.hlsl
+++ DirectXGame/PixelShader.hlsl
@@ -6,6 +6,8 @@ sampler TextureSampler: register(s0);
 struct PS_INPUT {
 	float4 pos: SV_POSITION;
 	float2 texcoord : TEXCOORD0;
+	float3 normal: TEXCOORD1;
+	float3 direction_to_camera: TEXCOORD2;
 };
 
 cbuffer constant: register(b0)
@@ -13,10 +15,35 @@ cbuffer constant: register(b0)
 	row_major float4x4 m_world;
 	row_major float4x4 m_view;
 	row_major float4x4 m_proj;
-	unsigned int m_time;
+	float4 m_light_direction;
 };
 
 float4 psmain(PS_INPUT input) : SV_TARGET
 {
-	return Texture.Sample(TextureSampler, input.texcoord*0.5);
+	//Ambient light
+	float ka = 0.1;
+	float3 ia = float3(1.0, 1.0, 1.0);
+
+	float3 ambient_light = ka * ia;
+
+	//Diffuse light
+	float3 kd = 0.7;
+	float id = float3(1.0, 1.0, 1.0);
+	float amount_diffuse_light = max(0.0, dot(m_light_direction.xyz, input.normal));
+
+	float3 diffuse_light = kd * amount_diffuse_light * id;
+
+	//Specular light
+	float ks = 1.0;
+	float3 is = float3(1.0, 1.0, 1.0);
+	float3 reflected_light = reflect(m_light_direction.xyz, input.normal);
+	float shiness = 30.0;
+	float amount_specular_light = pow(max(0.0, dot(reflected_light, input.direction_to_camera)), shiness);
+
+	float3 specular_light = ks * amount_specular_light * is;
+
+	float3 final_light = ambient_light + diffuse_light + specular_light;
+
+
+	return float4(final_light, 1.0);
 }
